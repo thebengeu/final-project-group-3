a300fff58924d48c0432fec89b2eaf0d833c4db6
Camillus Gerard Cai <cgcai@qxcg.net>
2013-04-06 18:31:36 +0800

    Partial commit - Stream Discovery Manager.

---
+++ Channely/HLSStreamAdvertiser.h
@@ -0,0 +1,14 @@
+//
+//  HLSStreamAdvertiser.h
+//  Channely
+//
+//  Created by Camillus Gerard Cai on 6/4/13.
+//  Copyright (c) 2013 nus.cs3217. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@protocol HLSStreamAdvertiser <NSObject>
+- (void) setAdvertiserDictionary:(NSDictionary *)dict;
+
+@end
