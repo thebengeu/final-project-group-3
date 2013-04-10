abff8a218156cfbd98cadd9013f4a014ab595367
Eli James <ejames@pandamian.com>
2013-04-11 00:05:52 +0800

    Addition of CollectionViewController

---
+++ ChanCollectionViewController.h
@@ -0,0 +1,13 @@
+//
+//  ChanCollectionViewController.h
+//  Channely
+//
+//  Created by Cedric on 4/10/13.
+//  Copyright (c) 2013 nus.cs3217. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ChanCollectionViewController : UICollectionViewController
+
+@end
---
+++ ChanCollectionViewController.m
@@ -0,0 +1,42 @@
+//
+//  ChanCollectionViewController.m
+//  Channely
+//
+//  Created by Cedric on 4/10/13.
+//  Copyright (c) 2013 nus.cs3217. All rights reserved.
+//
+
+#import "ChanCollectionViewController.h"
+
+@interface ChanCollectionViewController () {
+    NSArray *posts;
+}
+
+@end
+
+@implementation ChanCollectionViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	
+    // Initialize posts array
+    
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
