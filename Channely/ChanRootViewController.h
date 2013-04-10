a9d4fd1c72e5a960702208a55ada1f055db63b21
Camillus Gerard Cai <cgcai@qxcg.net>
2013-04-10 17:38:32 +0800

    Removed unnecessary methods in Root View Controller.

--- Channely/ChanRootViewController.h
+++ Channely/ChanRootViewController.h
@@ -7,13 +7,7 @@
 //
 
 #import <UIKit/UIKit.h>
-#import <HTTPServer.h>
-#import "HLSStreamDiscoveryManager.h"
-#import "HLSStreamAdvertiser.h"
-#import "ChanUtility.h"
-#import "HLSStreamSync.h"
 
-@interface ChanRootViewController : UINavigationController <HLSStreamAdvertiser>
-- (void) setAdvertiserDictionary:(NSDictionary *)dict;
+@interface ChanRootViewController : UINavigationController
 
 @end
--- Channely/ChanRootViewController.m
+++ Channely/ChanRootViewController.m
@@ -8,29 +8,13 @@
 
 #import "ChanRootViewController.h"
 
-static NSString *const cLocalServerLoadKey = @"_lsload";
-static NSString *const cApplicationTypeName = @"_channely._tcp.";
-static NSUInteger const cLocalServerPort = 80;
-//static NSString *const cHTMLDebugPage = @"<!DOCTYPE html><html><head><title>HLS Example</title></head><body><div><video src=\"prog_index.m3u8\" controls autoplay></video></div></body></html>";
-
 @interface ChanRootViewController ()
-// Internal.
-@property (strong) HTTPServer *_localServer;
-@property (strong) HLSStreamDiscoveryManager *_discoveryManager;
-
-- (void) setupHttpServer;
-- (void) setupDiscoveryManager;
-- (void) setupDirectories;
-- (void) setupStreamSync;
 
 @end
 
 @implementation ChanRootViewController
-// Internal.
-@synthesize _localServer;
-@synthesize _discoveryManager;
-
-- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil {
+#pragma mark View Controller Methods
+- (id) initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil {
     self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
     if (self) {
         
@@ -38,64 +22,19 @@ static NSUInteger const cLocalServerPort = 80;
     return self;
 }
 
-- (void)viewDidLoad {
+- (void) viewDidLoad {
     [super viewDidLoad];
 	// Do any additional setup after loading the view.
-    
-//    [self setupDirectories];
-//    
-//    [self setupHttpServer];
-//    [self setupDiscoveryManager];
-//    [self setupStreamSync];
+
 }
 
 - (void) viewDidAppear:(BOOL)animated {
     // DEBUG
-//    [[HLSStreamSync streamSync] syncStreamId:@"prog_index" playlistURL:[NSURL URLWithString:@"http://upthetreehouse.com/images/gear1/prog_index.m3u8"]];
 }
 
-- (void)didReceiveMemoryWarning {
+- (void) didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
-}
-
-#pragma mark Http Server
-- (void) setupHttpServer {
-    _localServer = [[HTTPServer alloc] init];
-    _localServer.port = cLocalServerPort;
-    _localServer.documentRoot = [ChanUtility webRootDirectory];
-    
-    _localServer.type = cApplicationTypeName;
-    
-    [_localServer start:nil];
-}
-
-#pragma mark HLS Stream Discovery Manager
-- (void) setupDiscoveryManager {
-    _discoveryManager = [HLSStreamDiscoveryManager discoveryManagerWithAdvertiser:self];
-}
-
-#pragma mark HLS Stream Advertiser Delegate
-- (void) setAdvertiserDictionary:(NSDictionary *)dict {
-    if (_localServer) {
-        NSUInteger load = [_localServer numberOfHTTPConnections] + [_localServer numberOfWebSocketConnections];
-        NSMutableDictionary *records = [NSMutableDictionary dictionaryWithDictionary:dict];
-        [records setObject:[NSString stringWithFormat:@"%d", load] forKey:cLocalServerLoadKey];
-        [_localServer setTXTRecordDictionary:records];
-    }
-}
-
-#pragma mark Recording Temp Directory
-- (void) setupDirectories {
-    // Note: this also clears all exisiting files in the web root.
-    [ChanUtility createDirectory:[ChanUtility webRootDirectory]];
-    
-    [ChanUtility createDirectory:[ChanUtility videoTempDirectory]];
-}
-
-#pragma mark HLS Stream Sync
-- (void) setupStreamSync {
-    [HLSStreamSync setupStreamSyncWithBaseDirectory:[ChanUtility webRootDirectory]];
 }
 
 @end
