a7c142d64f4c14c79eee6baf73cfb80b7c45b053
ahbeng <ah@beng.me>
2013-04-16 22:22:58 +0800

    Extract color constants into UIColor category

---
+++ UIColor+Channely.m
@@ -0,0 +1,23 @@
+//
+//  UIColor+Channely.m
+//  Channely
+//
+//  Created by Beng on 16/4/13.
+//  Copyright (c) 2013 nus.cs3217. All rights reserved.
+//
+
+#import "UIColor+Channely.h"
+
+@implementation UIColor (Channely)
+
++ (UIColor *)channelyGray
+{
+    return [UIColor colorWithWhite:0.75f alpha:1.0f];
+}
+
++ (UIColor *)channelyRed
+{
+    return [UIColor colorWithRed:0.792 green:0.031 blue:0.078 alpha:1];
+}
+
+@end
