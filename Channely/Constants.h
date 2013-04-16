ceee11cd174c1f34e4d03ba5d5a9c0047c5e6364
ahbeng <ah@beng.me>
2013-04-16 17:09:54 +0800

    Introduce Constants.h with kChannelyRed

---
+++ Channely/Constants.h
@@ -0,0 +1,9 @@
+//
+//  Constants.h
+//  Channely
+//
+//  Created by Beng on 16/4/13.
+//  Copyright (c) 2013 nus.cs3217. All rights reserved.
+//
+
+#define kChannelyRed [UIColor colorWithRed:0.792 green:0.031 blue:0.078 alpha:1]
