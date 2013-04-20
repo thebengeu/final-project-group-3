95ac786dc22ae0bb0d242497a25b62bbbb5b5f5d
Camillus Gerard Cai <cgcai@qxcg.net>
2013-04-20 12:02:12 +0800

    Modified load balancing algorithm to test for reachability before returning a source.

--- Channely/HLSLoadBalancer.m
+++ Channely/HLSLoadBalancer.m
@@ -47,30 +47,52 @@ static NSString *const cLocalhost = @"127.0.0.1";
     }
     NSUInteger topChunkCount = [HLSLoadBalancer totalChunksFromChunkField:firstChunkField];
     
-    // If the recording is complete, we randomly pick a peer from the set of peers with the full recording.
+    
+    // If the recording is complete, we build the set of peers with the full recording.
     // Since the array is sorted, we scan through from index 1, stopping at the last index with a full recording.
-    // Otherwise, we randomly pick a peer whose chunk count is within a defined spread radius of the top chunk count.
+    // Otherwise, we build the set of peers whose chunk count is within a defined spread radius of the top chunk count.
     // Since the array is sorted, we scan through from index 1, stopping when the above condition fails for the
     // first time.
-    NSUInteger randLimit;
-    for (randLimit = 1; randLimit < result.count; randLimit++) {
+    NSMutableArray *sourcePool = [NSMutableArray array];
+    for (NSUInteger randLimit = 1; randLimit < result.count; randLimit++) {
         HLSNetServicePathChunkCountTuple *peer = (HLSNetServicePathChunkCountTuple *)[result objectAtIndex:randLimit];
         if (completeRecordingExists && peer.chunkCount != topChunkCount) {
             break;
         } else if ((topChunkCount - peer.chunkCount) > cMaxSpreadRadius) { // ( {difference} > cMaxSpreadRadius )
             break;
+        } else {
+            [sourcePool addObject:peer];
         }
     }
     
-    // Randomly pick a peer from the top n.
-    // arc4random_uniform returns a random unint less than limit
-    NSUInteger randIndex = arc4random_uniform(randLimit);
+    NSURL *selectedURL = nil;
+    BOOL peerReachable = NO;
+    while (!peerReachable) {
+        // If there are no more reachable peers left from the array, return the default source.
+        if (sourcePool.count == 0) {
+            return serverSource;
+        }
+        
+        // Otherwise, randomly pick a peer from the set of available sources.
+        // arc4random_uniform returns a random unint less than limit
+        NSUInteger randIndex = arc4random_uniform(sourcePool.count);
+        HLSNetServicePathChunkCountTuple *selectedPeer = [result objectAtIndex:randIndex];
+        NSString *hostIpAddr = [HLSPeerDiscovery dottedDecimalFromNetService:selectedPeer.netService];
+        NSString *urlStr = [NSString stringWithFormat:cURLFormat, hostIpAddr, cHttpPort, selectedPeer.relativePath];
+        
+        // Test if the peer is reachable by attempting to download the playlist.
+        selectedURL = [NSURL URLWithString:urlStr];
+        NSError *error;
+        if ([selectedURL checkResourceIsReachableAndReturnError:&error]) {
+            NSLog(@"playlist:%@ was reachable. returning."); // DEBUG.
+            peerReachable = YES;
+        } else {
+            NSLog(@"playlist could not be reached. error:%@", error); // DEBUG.
+            [sourcePool removeObjectAtIndex:randIndex];
+        }
+    }
     
-    HLSNetServicePathChunkCountTuple *selectedPeer = [result objectAtIndex:randIndex];
-    NSString *hostIpAddr = [HLSPeerDiscovery dottedDecimalFromNetService:selectedPeer.netService];
-    NSString *urlStr = [NSString stringWithFormat:cURLFormat, hostIpAddr, cHttpPort, selectedPeer.relativePath];
-    
-    return [NSURL URLWithString:urlStr];
+    return selectedURL;
 }
 
 + (NSUInteger) totalChunksFromChunkField:(NSUInteger)chunkCount {
